62505c3ef9afa5704e5256806e0a9c851b5e30ad
shahid <mshahid90015@gmail.com>
2022-09-01 12:43:15 +0500

    addition to triangle problem

--- triangle_check.cpp
+++ triangle_check.cpp
@@ -4,13 +4,13 @@ int main()
 {
 // WRITE A PROGRAM WHETHER A TRIANGLE IS EQUILATERAL OR ISOSCELES OR SCALAR 
 int side1,side2,side3;
-cout<<"ENTER THE FIRST SIDE OF TRIANGLE "<<endl;
+cout<<"ENTER THE SIDE 1 OF TRIANGLE "<<endl;
 cin>>side1;
 
-cout<<"ENTER THE SECOND SIDE OF TRIANGLE "<<endl;
+cout<<"ENTER THE  SIDE  2 OF TRIANGLE "<<endl;
 cin>>side2;
 
-cout<<"ENTER THE THIRD SIDE OF TRIANGLE "<<endl;
+cout<<"ENTER THE SIDE 3 OF TRIANGLE "<<endl;
 cin>>side3;
 
 if(side1==side2 && side1==side3)
